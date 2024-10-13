a9d03090db1647c9548a69537e7cc69789c71d99
Hung <giaphung2k1@gmail.com>
2024-10-13 10:48:32 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -3,10 +3,11 @@ import "./style.css";
 
 // component = html + css + js
 const MyComponent = () => {
+    const name = "Giap Van Hung"; // String
 	return (
         // fragment
         <> 
-            <div className="">first component</div>
+            <div className="">{name}</div>
             <div className="child" style={
                 {borderRadius: '10px'}
                 }>Child</div>
