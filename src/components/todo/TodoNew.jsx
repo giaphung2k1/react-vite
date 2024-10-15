80edc33690d301c1b72363d0256c92a61871888c
Hung <giaphung2k1@gmail.com>
2024-10-15 07:58:48 +0700

    video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,10 +1,23 @@
 const TodoNew = (props) => {
-    const {addNewTodo} = props;
+    const { addNewTodo } = props;
     // addNewTodo('hungdz');
+    const handleClick = () => {
+        alert('click')
+    }
+
+    const handleOnChange= (name) =>{
+        console.log("change",name);
+        
+    }
     return (
         <div className='todo-new'>
-            <input type="text" />
-            <button>Add</button>
+            <input type="text"
+                onChange={(event) => handleOnChange(event.target.value)}
+            />
+            <button
+                style={{ cursor: "pointer" }}
+                onClick={handleClick}
+            >Add</button>
         </div>
     );
 }
