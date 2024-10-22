d7bd5c96ca0636e9fae278ffcd49806f1596f9a5
Hung <giaphung2k1@gmail.com>
2024-10-22 10:16:12 +0700

    video 45

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import TodoData from './components/todo/TodoData';
 import TodoNew from './components/todo/TodoNew';
 import reactLogo from './assets/react.svg';
 import { useState } from 'react';
+
+
 const App = () => {
 
 	const [todoList, setTodoList] = useState([]);
