a4a41da7a78a3454742a6fdbceb67919a8d87827
Hung <giaphung2k1@gmail.com>
2024-10-22 10:46:58 +0700

    video 47

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import TodoData from './components/todo/TodoData';
 import TodoNew from './components/todo/TodoNew';
 import reactLogo from './assets/react.svg';
 import { useState } from 'react';
+import Header from './components/layout/header';
+import Footer from './components/layout/footer';
 
 
 const App = () => {
@@ -29,25 +31,30 @@ const App = () => {
 	}
 
 	return (
-		<div className="todo-container">
-			<div className="todo-title">Todo List</div>
-			<TodoNew
-				addNewTodo={addNewTodo}
-			/>
-	
-				
-		
-			{todoList.length > 0 ?
-				<TodoData
-					todoList={todoList}
-					deleteToDo={deleteToDo}
+		<>
+			<Header/>
+			<div className="todo-container">
+				<div className="todo-title">Todo List</div>
+				<TodoNew
+					addNewTodo={addNewTodo}
 				/>
-			:
-				<div className="todo-image">
-					<img className='logo' src={reactLogo} alt="" />
-				</div>
-			}
-		</div>
+
+
+
+				{todoList.length > 0 ?
+					<TodoData
+						todoList={todoList}
+						deleteToDo={deleteToDo}
+					/>
+					:
+					<div className="todo-image">
+						<img className='logo' src={reactLogo} alt="" />
+					</div>
+				}
+			</div>
+			<Footer/>
+		</>
+
 
 	)
 }
---
+++ src/components/layout/footer.jsx
@@ -0,0 +1,10 @@
+import './footer.css';
+const Footer = () => {
+    return (
+        <div class="footer">
+            <p>React vip pro</p>
+        </div>
+    )
+}
+
+export default Footer;
