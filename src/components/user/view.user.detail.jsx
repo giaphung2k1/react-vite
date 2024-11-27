a1ad946db22e5b268c7feecb4d3e36578a09202e
Hung <giaphung2k1@gmail.com>
2024-11-27 17:03:28 +0700

    video83

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,19 +1,33 @@
-import { Button, Drawer } from "antd";
+import { Drawer } from "antd";
+import { useState } from "react";
 
 const ViewUserDetail = (props) => {
 
     const { setOpen, open, dataUserDetail, setDataUserDetail } = props;
+    const [file, setSelectedFile] = useState(null);
+
+
+    const preViewImage = (event) => {
+        if (!event.target.files || event.target.files.length === 0) {
+            setSelectedFile(undefined)
+            return
+        }
+        setSelectedFile(URL.createObjectURL(event.target.files[0]));
+
+    }
+
+    console.log(">>>>file", file);
 
 
     return (
         <div>
-            <Drawer 
-            title="Chi tiết User"
-            width={"40vw"}
-            onClose={() => {
-                setDataUserDetail(null)
-                setOpen(false)
-            }}
+            <Drawer
+                title="Chi tiết User"
+                width={"40vw"}
+                onClose={() => {
+                    setDataUserDetail(null)
+                    setOpen(false)
+                }}
                 open={open}>
                 {dataUserDetail ?
                     <>
@@ -21,29 +35,49 @@ const ViewUserDetail = (props) => {
                         <p>Full name: {dataUserDetail.fullName}</p>
                         <p>Email: {dataUserDetail.email}</p>
                         <p>Phone: {dataUserDetail.phone}</p>
-                        <div>
-                            <img 
-                            height={100}
-                            width={150}
-                            src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataUserDetail.avatar}`} alt="" />
+                        <div style={{
+                            marginTop: "10px",
+                            height: '100px', width: '150px',
+                            border: '1px solid #ccc'
+                        }}>
+                            <img
+                                height={100}
+                                width={150}
+                                src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataUserDetail.avatar}`} alt="" />
                         </div>
-                        
 
-                        <label 
-                        htmlFor="btnUpload"
-                        style={{
-                            display:"block",
-                            width:"fit-content",
-                            marginTop:"15px",
-                            padding:"5px 10px",
-                            background:"orange",
-                            borderRadius:"5px",
-                            cursor:"pointer"
-                        }}
+
+                        <label
+                            htmlFor="btnUpload"
+                            style={{
+                                display: "block",
+                                width: "fit-content",
+                                marginTop: "15px",
+                                padding: "5px 10px",
+                                background: "orange",
+                                borderRadius: "5px",
+                                cursor: "pointer"
+                            }}
                         >Upload avatar</label>
-                        <input type="file" hidden id="btnUpload"/>
-                   
-                        
+                        <input type="file"
+                            onChange={(event) => preViewImage(event)}
+                            name="avatar"
+                            hidden id="btnUpload" />
+
+                        {file &&
+                            <div style={{
+                                marginTop: "10px",
+                                height: '100px', width: '150px',
+                                border: '1px solid #ccc'
+                            }}>
+                                <img
+                                    height={100}
+                                    width={150}
+                                    src={file} alt="" />
+                            </div>
+                        }
+                       
+
                     </>
                     :
                     <>
